90a93c0cb4ace37f60df836ad3faf0865da81f4f
unknown <abs.junnior@hotmail.com>
2023-08-17 17:37:05 -0300

    arquivos env

--- bin/backend.dart
+++ bin/backend.dart
@@ -2,13 +2,19 @@ import 'package:shelf/shelf.dart';
 import 'api/blog_api.dart';
 import 'api/login_api.dart';
 import 'infra/custom_serve.dart';
+import 'utils/custom_env.dart';
 
 void main() async {
+  CustomEnv.fromFile('.env-dev');
   var cascedeHandler =
       Cascade().add(LoginApi().handler).add(BlogApi().handler).handler;
 
   var handler =
       Pipeline().addMiddleware(logRequests()).addHandler(cascedeHandler);
 
-  await CustomServe().initialize(handler);
+  await CustomServe().initialize(
+    handler: handler,
+    address: await CustomEnv.get<String>(key: 'server_address'),
+    port: await CustomEnv.get<int>(key: 'server_port'),
+  );
 }
