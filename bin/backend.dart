00e5c0d5bf808e72022b656c8c5728f36aabe872
Arnaldo Junior <abs.junnior@hotmail.com>
2023-08-16 21:47:32 -0300

    remodelação

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,11 +1,5 @@
-import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:backend/backend.dart' as backend;
 
-import 'server.dart';
+void main() {
 
-void main() async {
-  var _server = ServeHandler();
-
-  final server = await shelf_io.serve(_server.handler, "0.0.0.0", 8080);
-  print('O Site subiu -> http://localhost:8080 ');
 }
