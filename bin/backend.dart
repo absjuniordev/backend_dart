90d62b14dc49304616d40ba70f887522cc5818cf
Arnaldo Junior <abs.junnior@hotmail.com>
2023-08-16 22:28:34 -0300

    apis

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,5 +1,14 @@
-import 'package:backend/backend.dart' as backend;
+import 'package:shelf/shelf.dart';
+import 'api/blog_api.dart';
+import 'api/login_api.dart';
+import 'infra/custom_serve.dart';
 
-void main() {
+void main() async {
+  var cascedeHandler =
+      Cascade().add(LoginApi().handler).add(BlogApi().handler).handler;
 
+  var handler =
+      Pipeline().addMiddleware(logRequests()).addHandler(cascedeHandler);
+
+  await CustomServe().initialize(handler);
 }
